63d8c1a69745ac196b64acc49277b9113fa1317d
cnuebred <cnuebred@gmail.com>
2023-08-03 10:56:30 +0200

    update config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,24 @@
 module.exports = {
-  env: {
-    es2021: true,
-    node: true
-  },
-  extends: [
-    'standard'
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: [
-    '@typescript-eslint'
-  ],
-  rules: {
-    semi: 0,
-    camelcase: 0,
-    eqeqeq: 0
-  }
+    env: {
+        es2021: true,
+        node: true
+    },
+    extends: [
+        'standard'
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+    },
+    plugins: [
+        '@typescript-eslint'
+    ],
+    rules: {
+        indent: ['error', 4],
+        curly: 0,
+        semi: 0,
+        camelcase: 0,
+        eqeqeq: 0
+    }
 }
