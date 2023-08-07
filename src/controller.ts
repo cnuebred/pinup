708d86a73d60816c995b9e2584d8c7d903008423
Cube <46578659+cnuebred@users.noreply.github.com>
2023-08-07 13:58:32 +0200

    prettier - functions snake_case

--- src/controller.ts
+++ src/controller.ts
@@ -13,8 +13,8 @@ export function pin (
         throw new Error(`Parent class '${ParentClass.name}' is not assignable to parameter of Controller class`)
 
     return function <T extends Controller>
-    (originalMethod: T, context: ClassDecoratorContext): Controller {
-        const SubController: Controller = class extends originalMethod {
+    (original_method: T, context: ClassDecoratorContext): Controller {
+        const SubController: Controller = class extends original_method {
             name = context.name
             private_controller_key = true
             path = path
@@ -30,10 +30,10 @@ const pins_wrapper = (method: RequestMethod, path: string | string[]) => {
     return request_method_wrapper(method, one_or_many(path).many(''))
 }
 const request_method_wrapper = (request_method: RequestMethod, paths: string[]): any => {
-    return function (originalMethod: any, context: ClassMethodDecoratorContext<Controller>) {
-        function replacementMethod ({ rec, rep, op }: Pinpack) {
+    return function (original_method: any, context: ClassMethodDecoratorContext<Controller>) {
+        function replacement_method ({ rec, rep, op }: Pinpack) {
             op.pin.log()
-            const context = originalMethod.bind(this)
+            const context = original_method.bind(this)
             context({ rec, rep, op })
         }
 
@@ -45,41 +45,16 @@ const request_method_wrapper = (request_method: RequestMethod, paths: string[]):
                     name: context.name.toString(),
                     path: one_or_many(path).many(''),
                     parent: this,
-                    foo: replacementMethod,
+                    foo: replacement_method,
                     data: { ...this.data } || {}
                 })
             })
             this.data = {}
         })
 
-        return replacementMethod
+        return replacement_method
     }
 }
-
-// const request_method_wrapper = (request_method: RequestMethod, paths: string[]): any => {
-//     return function (target: Controller, name: string, descriptor: PropertyDescriptor) {
-//         if (!target.methods) target.methods = []
-//         const method = descriptor.value
-
-//         descriptor.value = function ({ rec, rep, op }: Pinpack) {
-//             op.pin.log()
-//             const context = method.bind(this)
-//             context({ rec, rep, op })
-//         }
-//         // console.log(paths)
-//         paths.forEach(path => {
-//             target.methods.push({
-//                 method: request_method,
-//                 name,
-//                 path: one_or_many(path).many(''),
-//                 parent: target,
-//                 foo: descriptor.value,
-//                 data: { ...target.data } || {}
-//             })
-//         })
-//         target.data = {}
-//     }
-// }
 
 export const pins = Object.fromEntries(PINS_METHODS.map((item: RequestMethod) => {
     return [item, (...path: string[]) => pins_wrapper(item, path)]
@@ -87,8 +62,8 @@ export const pins = Object.fromEntries(PINS_METHODS.map((item: RequestMethod) =>
 })) as { [K in RequestMethod]: (...path: string[]) => any }
 
 const data_method_wrapper = (name_dataset: 'params' | 'query' | 'body' | 'headers', keys: string[]): any => {
-    return (originalMethod: any, context: ClassMethodDecoratorContext<Controller>) => {
-        function replacementMethod ({ rec, rep, op }: Pinpack) {
+    return (original_method: any, context: ClassMethodDecoratorContext<Controller>) => {
+        function replacement_method ({ rec, rep, op }: Pinpack) {
             const req_dataset = rec[name_dataset]
             const require = []
             const dataset = keys.map(item => {
@@ -109,7 +84,7 @@ const data_method_wrapper = (name_dataset: 'params' | 'query' | 'body' | 'header
             }
 
             op[name_dataset] = { ...op[name_dataset], ...Object.fromEntries(dataset) }
-            const context = originalMethod.bind(this)
+            const context = original_method.bind(this)
             context({ rec, rep, op })
         }
         context.addInitializer(function () {
@@ -117,7 +92,7 @@ const data_method_wrapper = (name_dataset: 'params' | 'query' | 'body' | 'header
             this.data[name_dataset] = keys
         })
 
-        return replacementMethod
+        return replacement_method
     }
 }
 
@@ -129,8 +104,8 @@ export const need = {
 }
 
 export const auth = (error: boolean = true, jwt_secret?: string): any => {
-    return function (originalMethod: any, context: ClassMethodDecoratorContext<Controller>) {
-        function replacementMethod ({ rec, rep, op }: Pinpack) {
+    return function (original_method: any, context: ClassMethodDecoratorContext<Controller>) {
+        function replacement_method ({ rec, rep, op }: Pinpack) {
             if (!rec.headers.authorization)
                 return op.pin.res(
                     reply('This endpoint require \'header\' with specific properties: authorization')
@@ -153,9 +128,9 @@ export const auth = (error: boolean = true, jwt_secret?: string): any => {
                     op.auth.passed = false
                 }
             }
-            const context = originalMethod.bind(this)
+            const context = original_method.bind(this)
             context({ rec, rep, options: op })
         }
-        return replacementMethod
+        return replacement_method
     }
 }
